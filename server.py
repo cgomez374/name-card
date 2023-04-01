0583697a7f29baea5ebef50551c42b2c4d415634
Carlos Gomez <CGomez374@yahoo.com>
2023-04-01 12:18:01 -0700

    added POST method to contact form

--- server.py
+++ server.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.get("/")
 def main():
     return render_template("index.html")
+
+
+@app.route("/", methods=["GET", "POST"])
+def contact_me_info():
+    msg_sent = False
+    if request.method == "POST":
+        if request.form:
+            msg_sent = True
+    return render_template("index.html", msg_sent=msg_sent)
+
